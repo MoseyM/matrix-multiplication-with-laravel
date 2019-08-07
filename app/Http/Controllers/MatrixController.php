6c5e9d3dfb01b16cd2322bb8757df4b8c8a617a7
margaret <mmoseyee@gmail.com>
2019-08-07 10:31:50 -0500

    1. Added the route /matrix to application 2. Created the MatrixController and method for the route response

---
+++ app/Http/Controllers/MatrixController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MatrixController extends Controller
+{
+    /**
+     * Creates a new matrix product from user input.
+     * 
+     * @param  Request $request
+     * @return string
+     */
+    public function getMatrixProduct(Request $request)
+    {
+        //validate the input
+
+        //multiply the matrix
+
+        //return the alpha parsed representation
+    }
+}
